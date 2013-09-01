66015dc40bf8799871596517b051a4f08d26605c
blacktail <mr.zhangping@gmail.com>
2013-09-01 23:16:07 +0800

    upadte

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,21 @@
 #! /bin/bash
 
+desc=`file /bin/cat`
+echo $desc
+i=`expr match "$desc" ".*64-bit.*"`
+echo $i
+
+if [ $i -lt 1 ]; then
+	PATH=$PATH:`pwd`/deploy/node-32/bin/	
+else
+	PATH=$PATH:`pwd`/deploy/node-64/bin/
+fi
+
+npm install forever
 npm install
 
 if [ ! -d "./logs" ]; then
-mkdir logs
+	mkdir logs
 fi
 
 ./startup_production.sh
